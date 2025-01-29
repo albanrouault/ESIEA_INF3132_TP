c90ba183fb0a3e85ac662004b14afad29dc37509
FarmVivi <farmvividev@gmail.com>
2025-01-29 15:12:07 +0100

    Add BurriedState class to represent a new monster state with turn management

---
+++ src/main/java/fr/esiea/inf3132tp2024/model/monster/state/BurriedState.java
@@ -0,0 +1,12 @@
+package fr.esiea.inf3132tp2024.model.monster.state;
+
+public class BurriedState extends MonsterState {
+    /**
+     * Constructor of the BurriedState class
+     *
+     * @param turnsLeft the number of turns left for the state
+     */
+    public BurriedState(int turnsLeft) {
+        super(turnsLeft);
+    }
+}
