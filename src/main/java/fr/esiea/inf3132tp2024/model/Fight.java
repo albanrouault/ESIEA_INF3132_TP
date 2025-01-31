87c809e6d0d47821794f13ff5b07d361f4198c24
FarmVivi <farmvividev@gmail.com>
2025-01-31 11:35:16 +0100

    Refactor GameStatistic class and add Fight class for improved game mechanics

---
+++ src/main/java/fr/esiea/inf3132tp2024/model/Fight.java
@@ -0,0 +1,62 @@
+package fr.esiea.inf3132tp2024.model;
+
+import fr.esiea.inf3132tp2024.model.monster.Monster;
+import fr.esiea.inf3132tp2024.model.terrain.Terrain;
+import fr.esiea.inf3132tp2024.view.play.game.Game;
+
+public class Fight {
+    private final Game game;
+    private final Terrain terrain;
+    private final Player playerOne;
+    private final Player playerTwo;
+    private Monster monsterPlayerOne;
+    private Monster monsterPlayerTwo;
+    private FightState state = FightState.STARTING;
+
+    public Fight(Game game) {
+        this.game = game;
+        this.terrain = game.getTerrain();
+        this.playerOne = game.getPlayerOne();
+        this.playerTwo = game.getPlayerTwo();
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public Terrain getTerrain() {
+        return terrain;
+    }
+
+    public Player getPlayerOne() {
+        return playerOne;
+    }
+
+    public Player getPlayerTwo() {
+        return playerTwo;
+    }
+
+    public Monster getMonsterPlayerOne() {
+        return monsterPlayerOne;
+    }
+
+    public void setMonsterPlayerOne(Monster monsterPlayerOne) {
+        this.monsterPlayerOne = monsterPlayerOne;
+    }
+
+    public Monster getMonsterPlayerTwo() {
+        return monsterPlayerTwo;
+    }
+
+    public void setMonsterPlayerTwo(Monster monsterPlayerTwo) {
+        this.monsterPlayerTwo = monsterPlayerTwo;
+    }
+
+    public FightState getState() {
+        return state;
+    }
+
+    public void setState(FightState state) {
+        this.state = state;
+    }
+}
--- src/main/java/fr/esiea/inf3132tp2024/model/GameStatistic.java
+++ src/main/java/fr/esiea/inf3132tp2024/model/GameStatistic.java
@@ -1,12 +1,6 @@
 package fr.esiea.inf3132tp2024.model;
 
-import fr.esiea.inf3132tp2024.utils.direction.Orientation;
-import fr.esiea.inf3132tp2024.view.api.common.component.HorizontalAlignment;
-import fr.esiea.inf3132tp2024.view.api.terminal.TColor;
-import fr.esiea.inf3132tp2024.view.api.terminal.component.TLabel;
-import fr.esiea.inf3132tp2024.view.api.terminal.component.TPanel;
-
-public class GameStatistic extends TPanel implements Comparable<GameStatistic> {
+public class GameStatistic implements Comparable<GameStatistic> {
     private final long seed;
 
     private String playerOneName;
@@ -27,106 +21,14 @@ public class GameStatistic extends TPanel implements Comparable<GameStatistic> {
     }
 
     public GameStatistic(String playerOneName, String playerTwoName, int score, long seed) {
-        super(HorizontalAlignment.CENTER, Orientation.VERTICAL, 0);
-
         this.seed = seed;
         this.playerOneName = playerOneName;
         this.playerTwoName = playerTwoName;
         this.score = score;
-
-        this.update();
-    }
-
-    private void update() {
-        this.getComponents().clear();
-        int labelLength = 0;
-        int valueLength = 0;
-
-        // Nom des valeurs
-        TLabel playerOneLabel = new TLabel(HorizontalAlignment.RIGHT, "Joueur 1:");
-        playerOneLabel.getColors().add(TColor.GREEN);
-        if (playerOneLabel.getLength() > labelLength) {
-            labelLength = playerOneLabel.getLength();
-        }
-        TLabel playerTwoLabel = new TLabel(HorizontalAlignment.RIGHT, "Joueur 2:");
-        playerTwoLabel.getColors().add(TColor.GREEN);
-        if (playerTwoLabel.getLength() > labelLength) {
-            labelLength = playerTwoLabel.getLength();
-        }
-        TLabel scoreLabel = new TLabel(HorizontalAlignment.RIGHT, "Score:");
-        scoreLabel.getColors().add(TColor.GREEN);
-        if (scoreLabel.getLength() > labelLength) {
-            labelLength = scoreLabel.getLength();
-        }
-        TLabel seedLabel = new TLabel(HorizontalAlignment.RIGHT, "Graine utilisée:");
-        seedLabel.getColors().add(TColor.GREEN);
-        if (seedLabel.getLength() > labelLength) {
-            labelLength = seedLabel.getLength();
-        }
-
-        playerOneLabel.setLength(labelLength);
-        playerTwoLabel.setLength(labelLength);
-        scoreLabel.setLength(labelLength);
-        seedLabel.setLength(labelLength);
-
-        // Valeurs
-        TLabel playerOneValue = new TLabel(HorizontalAlignment.LEFT, this.playerOneName);
-        playerOneValue.getColors().add(TColor.YELLOW);
-        if (playerOneValue.getLength() > valueLength) {
-            valueLength = playerOneValue.getLength();
-        }
-        TLabel playerTwoValue = new TLabel(HorizontalAlignment.LEFT, this.playerTwoName);
-        playerTwoValue.getColors().add(TColor.YELLOW);
-        if (playerTwoValue.getLength() > valueLength) {
-            valueLength = playerTwoValue.getLength();
-        }
-        TLabel scoreValue = new TLabel(HorizontalAlignment.LEFT, String.valueOf(this.score));
-        scoreValue.getColors().add(TColor.YELLOW);
-        if (scoreValue.getLength() > valueLength) {
-            valueLength = scoreValue.getLength();
-        }
-        TLabel seedValue = new TLabel(HorizontalAlignment.LEFT, String.valueOf(this.seed));
-        seedValue.getColors().add(TColor.YELLOW);
-        if (seedValue.getLength() > valueLength) {
-            valueLength = seedValue.getLength();
-        }
-
-        playerOneValue.setLength(valueLength);
-        playerTwoValue.setLength(valueLength);
-        scoreValue.setLength(valueLength);
-        seedValue.setLength(valueLength);
-
-        // Panels pour chaque champ
-        TPanel playerOnePanel = new TPanel(HorizontalAlignment.CENTER, Orientation.HORIZONTAL, 1);
-        playerOnePanel.getComponents().add(playerOneLabel);
-        playerOnePanel.getComponents().add(playerOneValue);
-        playerOnePanel.autoResize();
-        this.getComponents().add(playerOnePanel);
-
-        TPanel playerTwoPanel = new TPanel(HorizontalAlignment.CENTER, Orientation.HORIZONTAL, 1);
-        playerTwoPanel.getComponents().add(playerTwoLabel);
-        playerTwoPanel.getComponents().add(playerTwoValue);
-        playerTwoPanel.autoResize();
-        this.getComponents().add(playerTwoPanel);
-
-        TPanel scorePanel = new TPanel(HorizontalAlignment.CENTER, Orientation.HORIZONTAL, 1);
-        scorePanel.getComponents().add(scoreLabel);
-        scorePanel.getComponents().add(scoreValue);
-        scorePanel.autoResize();
-        this.getComponents().add(scorePanel);
-
-        TPanel seedPanel = new TPanel(HorizontalAlignment.CENTER, Orientation.HORIZONTAL, 1);
-        seedPanel.getComponents().add(seedLabel);
-        seedPanel.getComponents().add(seedValue);
-        seedPanel.autoResize();
-        this.getComponents().add(seedPanel);
-
-        this.autoResize();
     }
 
     public void activeCheat() {
         this.cheatModeActivated = true;
-        this.update();
     }
 
     public boolean isCheatModeActivated() {
@@ -140,7 +42,6 @@ public class GameStatistic extends TPanel implements Comparable<GameStatistic> {
      */
     public void setPlayerOneName(String playerName) {
         this.playerOneName = playerName;
-        this.update();
     }
 
     /**
@@ -150,7 +51,6 @@ public class GameStatistic extends TPanel implements Comparable<GameStatistic> {
      */
     public void setPlayerTwoName(String playerName) {
         this.playerTwoName = playerName;
-        this.update();
     }
 
     /**
@@ -158,7 +58,6 @@ public class GameStatistic extends TPanel implements Comparable<GameStatistic> {
      */
     public void calculScore() {
         this.score = 0;
-        this.update();
     }
 
 
