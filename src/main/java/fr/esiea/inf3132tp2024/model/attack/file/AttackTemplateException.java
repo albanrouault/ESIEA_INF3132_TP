4673c84cebb704e690e0196c70232c19f5ac5c3a
Banban <alban.rouault@hotmail.com>
2025-01-31 22:08:38 +0100

    Exception AttackTemplate

---
+++ src/main/java/fr/esiea/inf3132tp2024/model/attack/file/AttackTemplateException.java
@@ -0,0 +1,12 @@
+package fr.esiea.inf3132tp2024.model.attack.file;
+
+public class AttackTemplateException extends Exception {
+    
+    public AttackTemplateException(String message) {
+        super(message);
+    }
+
+    public AttackTemplateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
